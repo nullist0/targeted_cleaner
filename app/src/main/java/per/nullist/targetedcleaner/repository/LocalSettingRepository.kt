96b362e893ed0d231b8e800f5557b7f453b90270
Lee-Null <nullist.null@gmail.com>
2021-03-10 00:32:17 +0900

    implement LocalSettingRepository.kt

--- app/src/main/java/per/nullist/targetedcleaner/repository/LocalSettingRepository.kt
+++ app/src/main/java/per/nullist/targetedcleaner/repository/LocalSettingRepository.kt
@@ -1,16 +1,45 @@
 package per.nullist.targetedcleaner.repository
 
 import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.SystemClock
+import per.nullist.targetedcleaner.component.activity.MainActivity
 import per.nullist.targetedcleaner.entity.SettingRepository
 
 class LocalSettingRepository(private val context: Context) : SettingRepository {
+    // TODO: it should not be defined in this file
+    private val REQUEST_CODE = 10
+
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    private fun getInterval() : Long =
+        SharedPreferenceHelper
+            .instance(context)
+            .run { getLong("interval", 0L) }
+
+    private inline fun <reified T> buildIntent(context: Context) : Intent = Intent(context, T::class.java)
+
+    private fun buildPendingIntent(): PendingIntent {
+        val intent = buildIntent<MainActivity>(context)
+        return PendingIntent.getBroadcast(context, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
     override fun setTimer(milliInterval: Int) {
-        val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
+        alarmManager.run {
+            val interval = getInterval()
+            val triggerAtMills = SystemClock.elapsedRealtime() + interval
+            setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME,
+                triggerAtMills,
+                interval,
+                buildPendingIntent()
+            )
+        }
     }
 
-    override fun deleteTimer() {
-        TODO("Not yet implemented")
-    }
+    override fun deleteTimer() = alarmManager.cancel(buildPendingIntent())
 }
