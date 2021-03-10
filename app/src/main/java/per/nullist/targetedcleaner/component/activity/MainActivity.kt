9c78b29c980e2de865f9a2cb5fd99c46c81b9177
Lee-Null <nullist.null@gmail.com>
2021-03-10 20:53:14 +0900

    feat: update repositories

--- app/src/main/java/per/nullist/targetedcleaner/component/activity/MainActivity.kt
+++ app/src/main/java/per/nullist/targetedcleaner/component/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package per.nullist.targetedcleaner.component.activity
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
